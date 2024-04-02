166bac856b07590f3fde4320776b9f12efe2658d
Ang Chen <chenang@outlook.com>
2024-04-02 19:04:50 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,9 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const pageStyles = {
-    color: "#232129",
-    padding: "96px",
-    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+color: "#232129",
+padding: "96px",
+fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
 marginTop: 0,
