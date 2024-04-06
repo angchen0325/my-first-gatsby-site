6c42db3e7013e055db0462ae363efa5bb55b3236
Ang Chen <chenang@outlook.com>
2024-04-06 16:38:08 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,40 +1,35 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
-const pageStyles = {
-color: "#232129",
-padding: "96px",
-fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-marginTop: 0,
-marginBottom: 64,
-maxWidth: 320,
-}
+// const pageStyles = {
+// color: "#232129",
+// padding: "96px",
+// fontFamily: "-apple-system, Roboto, sans-serif, serif",
+// }
+// const headingStyles = {
+// marginTop: 0,
+// marginBottom: 64,
+// maxWidth: 320,
+// }
 
-const paragraphStyles = {
-marginBottom: 48,
-}
+// const paragraphStyles = {
+// marginBottom: 48,
+// }
 
 
 const AboutPage = () => {
     return (
-        <main style={pageStyles}>
-            <h1 style={headingStyles}>About Me</h1>
+        <main>
+            <h1>About Me</h1>
             {/* <Link to="/">Back to Home</Link>. */}
-            <p style={paragraphStyles}>
+            <p>
                 Hi there! I'm the proud creator of this site, which I built with Gatsby.
             </p>
         </main>
     )
 }
 
+
+export const Head = () => <title>About Me</title>
+
 export default AboutPage
-
-// export const Head = () => (
-//     <>
-//     <title>About Me</title>
-//     <meta name="description" content="Your description"/>
-//     </>
-
-// )
