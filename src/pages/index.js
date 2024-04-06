8f161f641562a761ab9ee95a47c57c92e984bb9b
Ang Chen <chenang@outlook.com>
2024-04-06 23:10:54 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 // import Layout from "../components/layout"
 
 // const pageStyles = {
@@ -182,7 +182,7 @@ const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
-      {/* <Link to='/about'>About Me</Link>  */}
+      <Link to='/about'>About</Link> 
       <p>I'm making this by following the Gatsby Tutorial.
       </p>
     </main>
