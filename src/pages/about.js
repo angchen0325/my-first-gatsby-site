f8b57601b10279baab6308b8ea1010a6eb9c7db0
Ang Chen <chenang@outlook.com>
2024-04-07 23:11:44 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 // const pageStyles = {
 // color: "#232129",
@@ -19,13 +19,16 @@ import { Link } from "gatsby"
 
 const AboutPage = () => {
     return (
-        <main>
-            <h1>About Me</h1>
-            <Link to="/">Back to Home</Link>.
-            <p>
-                Hi there! I'm the proud creator of this site, which I built with Gatsby.
-            </p>
-        </main>
+        // <main>
+        //     <h1>About Me</h1>
+        //     <Link to="/">Back to Home</Link>.
+        //     <p>
+        //         Hi there! I'm the proud creator of this site, which I built with Gatsby.
+        //     </p>
+        // </main>
+        <Layout pageTitle="About Me">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
     )
 }
 
